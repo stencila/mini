49c630f856c24a68a4d16aa457538e9aba294c82
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-03-11 00:07:37 +1300

    Removed examples.

--- examples/example.js
+++
@@ -1,5 +0,0 @@
-import ExampleSheetEngine from './ExampleSheetEngine'
-import ExampleSheetComponent from './ExampleSheetComponent'
-import scope from './example-scope'
-
-export { ExampleSheetEngine, ExampleSheetComponent, scope }
