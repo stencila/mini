d5de52eacd05f4560543364cc2f6b08bee28671d
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-05-30 10:10:17 +0200

    Use testAsync helper

--- test/testHelpers.js
+++ test/testHelpers.js
@@ -1,4 +1,20 @@
+import { test } from 'substance-test'
 import { DefaultDOMElement, platform } from 'substance'
+
+export function testAsync (name, func) {
+  test(name, async assert => {
+    let success = false
+    try {
+      await func(assert)
+      success = true
+    } finally {
+      if (!success) {
+        assert.fail('Test failed with an uncaught exception.')
+        assert.end()
+      }
+    }
+  })
+}
 
 export function wait(ms) {
   return () => {
